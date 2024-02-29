e3c46d95aaa25dfc21fb7178a61aeb1d3d8f50cd
kc-code32 <kelvinchen138@gmail.com>
2024-02-29 17:27:36 -0600

    removed html and console.log

--- server/server.js
+++ server/server.js
@@ -41,7 +41,6 @@ app.use('/build', express.static(path.join(__dirname, '../build')));
 
 // serve index.html on the route '/'
 app.get('/**', (req, res) => {
-  console.log('try to load');
   return res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
 
