25dffdb41797d1d70013084eee8284efc4ddbedf
kc-code32 <kelvinchen138@gmail.com>
2024-02-29 15:25:30 -0600

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,38 +5,12 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
 module.exports = {
+  mode: process.env.NODE_ENV,
   entry : './client/index.js',
   output : {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/',
     filename: 'bundle.js',
-  },
-  devtool: 'eval-source-map',
-  mode: process.env.NODE_ENV,
-  devServer: {
-    static: {
-      // match the output path
-      directory: path.resolve(__dirname, 'dist'),
-      // match the output 'publicPath'
-      publicPath: '/',
-    },
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    /**
-     * proxy is required in order to make api calls to
-     * express server while using hot-reload webpack server
-     * routes api fetch requests from localhost:8080/api/* (webpack dev server)
-     * to localhost:3000/api/* (where our Express server is running)
-     */
-    proxy: {
-      '/assets/**': {
-        target: 'http://localhost:3000/',
-        secure: false,
-      },
-      '/server/**': {
-        target: 'http://localhost:3000/',
-        secure: false,
-      },
-    },
   },
   module: {
     rules: [
@@ -54,30 +28,62 @@ module.exports = {
         }
       },
       {
-        // test: /\.s[ac]ss$/i, 
-        test: /\.s?css/, 
-        // test: /\.(css)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] // MiniCssExtractPlugin.loader to style-loader
+        test: /\.s?css/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
+      // {
+      //   // test: /\.s[ac]ss$/i, 
+      //   test: /\.s?css/, 
+      //   // test: /\.(css)$/,
+      //   use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] // MiniCssExtractPlugin.loader to style-loader
+      // },
     ]
   },
+  devtool: 'eval-source-map',
+  devServer: {
+    static: {
+      // match the output path
+      directory: path.resolve(__dirname, 'dist'),
+      // match the output 'publicPath'
+      publicPath: '/',
+    },
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    historyApiFallback: true,
+    /**
+     * proxy is required in order to make api calls to
+     * express server while using hot-reload webpack server
+     * routes api fetch requests from localhost:8080/api/* (webpack dev server)
+     * to localhost:3000/api/* (where our Express server is running)
+     */
+    proxy: {
+      '/assets/**': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      },
+      '/server/**': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      },
+    },
+  },
+  
   // plugins: [
-  //   new HtmlWebpackPlugin ({
-  //     title: 'Development',
-  //     template: 'index.html'
+  //   new MiniCssExtractPlugin(),
+  //   new HtmlWebpackPlugin({
+  //     template: './client/assets/index.html',
   //   }),
   // ],
   plugins: [
-    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
-      template: './client/assets/index.html',
+      title: 'Development',
+      template: './index.html',
     }),
   ],
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
     maxAssetSize: 512000
-},
+  },
   resolve: {
     // Enable importing JS / JSX files without specifying their extension
     extensions: ['.js', '.jsx'],
